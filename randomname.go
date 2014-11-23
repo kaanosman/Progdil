1c8353db943a66ef6bb147a5249be001d87c5e72
kaanosman <osman.arslan@bil.omu.edu.tr>
2014-11-23 21:48:50 +0200

    eksikliklerin giderilmesi

--- randomname.go
+++ randomname.go
@@ -1,16 +1,17 @@
 package main
 
-import ("fmt"
+import (
+"fmt"
 "math/rand"
 "time"
 "flag"
 )
+
 func seedAndReturnRandom(n int) int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(n)
 }
 
-// Returns a random part of a slice
 func randomFrom(source []string) string {
 	return source[seedAndReturnRandom(len(source))]
 }
@@ -18,36 +19,34 @@ func randomFrom(source []string) string {
 func main() {
 
  flag.Parse()
-  dil := flag.Arg(0)
-var adet int = flag.Arg(1)
-var x int =int(adet)
+ dil := flag.Arg(0)
+ adet := flag.Arg(1)
   
-isim:= []string {"kedi","insan","deniz","masa","dolap","bilgisayar",
+ isim:= []string {"kedi","insan","deniz","masa","dolap","bilgisayar",
 			"kapi","pencere","hafta","gece","gunduz","marti","kazak",
 			"sarisin","sahil","merdiven","ahmet","yol","araba","agac",
 			"yaprak","dal","adam","konuk","ahmet","koca","kadin"}
 
 
-sifat:= []string {"deli", "dolu" , "yaramaz","iyi","guzel","hizli","yavas","yakisikli","mert",
+ sifat:= []string {"deli", "dolu" , "yaramaz","iyi","guzel","hizli","yavas","yakisikli","mert",
 			"cimri","kel","masmavi","ucan","kac","yarim","bu","su"}
-name:= []string {"joe", "mike" , "caitlyn","graves","soraka"}
+ name:= []string {"joe", "mike" , "caitlyn","graves","soraka"}
 
-adjactive:= []string {"happy", "sad" , "beautiful"}
+ adjactive:= []string {"happy", "sad" , "beautiful"}
 
 
 
 if (dil=="tr") {
-   
+   for i := 0 ; i < adet ; i++ {
 fmt.Println(randomFrom(sifat),randomFrom(isim))
-
-                        
+  }                        
 }
+
 if (dil=="en") {
-    
+   for i := 0 ; i < adet ; i++ { 
 
 fmt.Println(randomFrom(adjactive),randomFrom(name))
-
-                         
+  }
 }
 
 
